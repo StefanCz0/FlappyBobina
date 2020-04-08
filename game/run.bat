43d96049ac963e9a65206b119799bbc72718ed11
StefanCz0 <59145711+StefanCz0@users.noreply.github.com>
2020-04-08 22:42:33 +0200

    Update run.bat

--- game/run.bat
+++ game/run.bat
@@ -1,2 +1,11 @@
+@echo off
+echo **************************************************
+echo Github: https://github.com/StefanCz0/FlappyBobina
+echo Author: StefanCz0
+echo Do not copy
+echo Enjoy play!
+echo **************************************************
+PAUSE
 java -jar flappybobina.jar
+echo Goodbye
 PAUSE
